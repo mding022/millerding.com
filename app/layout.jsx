00db31ba6d2fef73b89932e472c8847dd660ed69
millerding <mding022@uottawa.ca>
2024-09-17 19:48:09 -0400

    font change

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,15 @@
 // This is the root layout component for your Next.js app.
 // Learn more: https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts#root-layout-required
-import { DM_Sans } from 'next/font/google'
+import { Poppins } from 'next/font/google'
 import { Space_Mono } from 'next/font/google'
 import { cn } from '@/lib/utils'
 import './globals.css'
 
-const fontHeading = DM_Sans({
+const fontHeading = Poppins({
     subsets: ['latin'],
     display: 'swap',
     variable: '--font-heading',
+    weight: ['100','200','300','400','500','600','700','800','900']
 })
 
 const fontBody = Space_Mono({
